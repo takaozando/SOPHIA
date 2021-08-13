49ccbb2175cc46d314f9de1e4f6a41c5444eebc3
takaozando <gustavo_hideo@hotmail.com>
2021-08-12 22:00:49 -0300

    PingPong

---
+++ SOPHIA/Commands/pingpong.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOPHIA.Commands
+{
+    public class PingPong : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+
+        public async Task pingPong()
+        {
+            await ReplyAsync("pong");
+        }
+    }
+}
--- SOPHIA/Program.cs
+++ SOPHIA/Program.cs
@@ -1,62 +1,91 @@
-﻿using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.EventArgs;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace SOPHIA
+namespace TutorialBot
 {
     class Program
     {
-        private DiscordClient _client;
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
 
-        static void Main(string[] args) => new Program().RodarBotAsync().GetAwaiter().GetResult();
-        {
-    
-        public async Task RodarBotAsync()
-        {
-            DiscordConfiguration discordConfig = new DiscordConfiguration
-            {
-                Token = "Token",
-                TokenType = TokenType.Bot, //sempre Bot
-                ReconnectIndefinitely = true,
-                GatewayCompressionLevel = GatewayCompressionLevel.Stream,
-                AutoReconnect = true,
-            };
-            _client = new DiscordClient(discordConfig);
+
+
+            string tokenBot = "ODc1MTkwNTUxNjI3MDQyODI2.YRR6tw.s8vtuSPPocXblf7b_L9VITYlB14";
+
+            //assinatura de eventos
+
             _client.Ready += Client_Ready;
-            _client.ClientErrored += Client_ClientError;
+            _client.Log += ClientLog;
+            _client.UserJoined += UserJoined;
 
-            string[] prefix = new string[1];
-            prefix[0] = "!";
+            
+            await Client_Ready();
+            await ComandosBot();
 
-            CommandsNextExtension cnt = _client.UseCommandsNext(new CommandsNextConfiguration()
-            {
-                StringPrefixes = prefix,
-                EnableDms = true, //Mensagem privada
-                CaseSensitive = false, //Comandos case sensitive
-                EnableDefaultHelp = false,
-                EnableMentionPrefix = true, //habilita mencionar o bot
-                IgnoreExtraArguments = true //Ignora char a mais após comando [ex: !join = !joinnn =!join9]
-            });
-            discordConfig.CommandExecuted += Cnt_CommandExecuted;
+            _client.LoginAsync(TokenType.Bot, tokenBot);
 
-            await _client.ConnectAsync();
-            await Task.Delay(-1); //
+            await _client.StartAsync();
 
-        }
-        private Task Client_Ready(ReadyEventArgs e)
-        {
-            e.
-            _client.UpdateStatusAsync(new DSharpPlus.Entities.DiscordActivity("!ajuda",DSharpPlus.Entities.ActivityType.Playing),DSharpPlus.Entities.UserStatus.Online)
-            return Task.CompletedTask;
-        }
-
-        private Task Client_ClientError(ClientErrorEventArgs e)
-        {
+            await Task.Delay(-1);
             
         }
 
 
+        private async Task UserJoined(SocketGuildUser user)
+        {
+            var usuario = user.Guild;
+
+        }
+
+        private Task ClientLog(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        private async Task Client_Ready()
+        {
+            await _client.SetActivityAsync(new Game("BIG TESTER", ActivityType.Playing, ActivityProperties.None));
+        }
+        public async Task ComandosBot()
+        {
+            _client.MessageReceived += IniciandoComandos;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+        }
+
+        public async Task IniciandoComandos(SocketMessage arg)
+        {
+            var mensagem = arg as SocketUserMessage;
+            if (mensagem is null || mensagem.Author.IsBot) return;
+
+            var Context = new SocketCommandContext(_client, mensagem);
+            int argPost = 0;
+            if(mensagem.HasStringPrefix(".",ref argPost))
+            {
+                var result = await _commands.ExecuteAsync(Context, argPost, _services);
+
+                if(!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
     }
 }
